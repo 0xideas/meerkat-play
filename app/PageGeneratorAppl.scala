d05c3d1617d85ce8d64d3591643a04c5e8ad3202
leon <leontimnaluithlen@gmail.com>
2021-03-19 11:41:37 +0100

    changed asset paths

--- app/PageGeneratorAppl.scala
+++ app/PageGeneratorAppl.scala
@@ -6,18 +6,18 @@ import ada.enhancements._
 import io.circe.generic.auto._, io.circe.syntax._
 
 object PageGeneratorAppl{
-    val food1 = "/images/headers/food1.jpeg"
-    val food2 = "/images/headers/food2.jpeg"
-    val gadget1 = "/images/headers/gadget1.jpeg"
-    val gadget2 = "/images/headers/gadget2.jpeg"
-    val pet1 = "/images/headers/pet1.jpeg"
-    val pet2 = "/images/headers/pet2.jpeg"
-    val trees1 = "/images/headers/tree1.jpeg"
-    val trees2 = "/images/headers/tree2.jpeg"
-    val neighbourhood1 = "/images/headers/neighbourhood1.jpeg"
-    val neighbourhood2 = "/images/headers/neghbourhood2.jpeg"
-    val friends1 = "/images/headers/friends1.jpeg"
-    val friends2 = "/images/headers/friends2.jpeg"
+    val food1 = "images/headers/food1.jpeg"
+    val food2 = "images/headers/food2.jpeg"
+    val gadget1 = "images/headers/gadget1.jpeg"
+    val gadget2 = "images/headers/gadget2.jpeg"
+    val pet1 = "images/headers/pet1.jpeg"
+    val pet2 = "images/headers/pet2.jpeg"
+    val trees1 = "images/headers/tree1.jpeg"
+    val trees2 = "images/headers/tree2.jpeg"
+    val neighbourhood1 = "images/headers/neighbourhood1.jpeg"
+    val neighbourhood2 = "images/headers/neghbourhood2.jpeg"
+    val friends1 = "images/headers/friends1.jpeg"
+    val friends2 = "images/headers/friends2.jpeg"
 
     case class Article(id: Int, headline: String, image: String)
     val h1 = List(List(Article(0, "99 Recipes for Lockdown", food1), Article(0, "Don't Move - Stay Healthy", food1) ),
