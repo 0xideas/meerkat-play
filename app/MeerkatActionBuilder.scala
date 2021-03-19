30b90da24baafb32c706e14e6a3c70b08fb83e96
leon <leontimnaluithlen@gmail.com>
2021-03-19 13:54:51 +0100

    changed asset paths

--- app/MeerkatActionBuilder.scala
+++ app/MeerkatActionBuilder.scala
@@ -105,11 +105,4 @@ case class MeerkatControllerComponents @Inject()(
 /**
   * Exposes actions and handler to the PostController by wiring the injected state into the base class.
   */
-class MeerkatBaseController @Inject()(pcc: MeerkatControllerComponents)
-    extends BaseController
-    with RequestMarkerContext {
-  override protected def controllerComponents: ControllerComponents = pcc
 
-  def PostAction: MeerkatActionBuilder = pcc.postActionBuilder
-
-}
