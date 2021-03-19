b7435268cbe92aa192e868cee25a0da0979392ca
leon <leontimnaluithlen@gmail.com>
2021-03-19 14:19:35 +0100

    added png images

--- app/PageGeneratorAppl.scala
+++ app/PageGeneratorAppl.scala
@@ -7,17 +7,17 @@ import io.circe.generic.auto._, io.circe.syntax._
 
 object PageGeneratorAppl{
     val food1 = "./public/images/food1.png"
-    val food2 = "./public/images/food2.jpeg"
-    val gadget1 = "./public/images/gadget1.jpeg"
-    val gadget2 = "./public/images/gadget2.jpeg"
-    val pet1 = "./public/images/pet1.jpeg"
-    val pet2 = "./public/images/pet2.jpeg"
-    val trees1 = "./public/images/tree1.jpeg"
-    val trees2 = "./public/images/tree2.jpeg"
-    val neighbourhood1 = "./public/images/neighbourhood1.jpeg"
-    val neighbourhood2 = "./public/images/neighbourhood2.jpeg"
-    val friends1 = "./public/images/friends1.jpeg"
-    val friends2 = "./public/images/friends2.jpeg"
+    val food2 = "./public/images/food2.png"
+    val gadget1 = "./public/images/gadget1.png"
+    val gadget2 = "./public/images/gadget2.png"
+    val pet1 = "./public/images/pet1.png"
+    val pet2 = "./public/images/pet2.png"
+    val trees1 = "./public/images/tree1.png"
+    val trees2 = "./public/images/tree2.png"
+    val neighbourhood1 = "./public/images/neighbourhood1.png"
+    val neighbourhood2 = "./public/images/neighbourhood2.png"
+    val friends1 = "./public/images/friends1.png"
+    val friends2 = "./public/images/friends2.png"
 
     case class Article(id: Int, headline: String, image: String)
     val h1 = List(List(Article(0, "99 Recipes for Lockdown", food1), Article(0, "Don't Move - Stay Healthy", food1) ),
